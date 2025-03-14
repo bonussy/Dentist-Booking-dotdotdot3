27b3d292a6a8d4410851767ee4ff7b6aa3fb52ac
Warintorn Surapolchai <poohmon.bonus@gmail.com>
2025-03-14 16:12:53 +0700

    connect DB

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
 
 const app = express();
 
@@ -11,4 +15,11 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
