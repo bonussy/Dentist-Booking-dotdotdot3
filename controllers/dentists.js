2db12ff1b88e8b9af0f4fc55c6f52286fa3d5a94
Warintorn Surapolchai <poohmon.bonus@gmail.com>
2025-03-14 21:04:55 +0700

    connect controller with mongoDB

--- controllers/dentists.js
+++ controllers/dentists.js
@@ -1,6 +1,19 @@
+const Dentist = require('../models/Dentist');
+
 //@desc Get all dentists
 //@rotue GET /api/v1/dentists
 //@access Public
-exports.getDentists = (req, res, next) => {
-    res.status(200).json({ success: true, msg: 'Show all dentists' });
+exports.getDentists = async (req, res, next) => {
+    try{
+        const dentists = await Dentist.find();
+
+        res.status(200).json({
+            success: true,
+            count: dentists.length,
+            data: dentists
+        });
+
+    } catch(err){
+        res.status(400).json({ success: false , message: err.message });
+    }
 };
